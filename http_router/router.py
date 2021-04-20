af5273decd07d9d21f3a30f48aad9dea007cf474
Kirill Klenov <horneds@gmail.com>
2021-04-20 15:45:47 +0300

    feat: support pypy

---
+++ http_router/router.py
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+import typing as t
+from collections import defaultdict
+from functools import partial, lru_cache
+
+from . import NotFound, RouterError, MethodNotAllowed  # noqa
+from .utils import parse_path
+from .typing import CBV, CB, TYPE_METHODS, TYPE_PATH
+
+
+class Router:
+    """Route HTTP queries."""
+
+    NotFound: t.ClassVar[t.Type[Exception]] = NotFound                  # noqa
+    RouterError: t.ClassVar[t.Type[Exception]] = RouterError            # noqa
+    MethodNotAllowed: t.ClassVar[t.Type[Exception]] = MethodNotAllowed  # noqa
+
+    def __init__(self, trim_last_slash: bool = False, validator: CBV = None):
+        """Initialize the router."""
+        self.trim_last_slash = trim_last_slash
+        self.validator = validator or (lambda v: True)
+        self.plain: t.DefaultDict[str, t.List[BaseRoute]] = defaultdict(list)
+        self.dynamic: t.List[BaseRoute] = list()
+
+    def __call__(self, path: str, method: str = "GET") -> RouteMatch:
+        """Found a target for the given path and method."""
+        if self.trim_last_slash:
+            path = path.rstrip('/')
+
+        match = self.match(path, method)
+        if not match.path:
+            raise self.NotFound(path, method)
+
+        if not match.method:
+            raise self.MethodNotAllowed(path, method)
+
+        return match
+
+    def __route__(self, root: Router, prefix: str, *paths: t.Any,
+                  methods: TYPE_METHODS = None, **params):
+        """Bind self as a nested router."""
+        route = Mount(prefix, router=self)
+        root.dynamic.insert(0, route)
+        return self
+
+    @lru_cache(maxsize=1024)
+    def match(self, path: str, method: str) -> RouteMatch:
+        """Search a matched target for the given path and method."""
+        neighbour = None
+        for route in self.plain.get(path, self.dynamic):
+            match = route.match(path, method)
+            if match.path:
+                if match.method:
+                    return match
+                neighbour = match
+
+        return RouteMatch(False, False) if neighbour is None else neighbour
+
+    def bind(self, target: t.Any, *paths: TYPE_PATH, methods: TYPE_METHODS = None, **opts):
+        """Bind a target to self."""
+        if opts:
+            target = partial(target, **opts)
+
+        if isinstance(methods, str):
+            methods = [methods]
+
+        if methods is not None:
+            methods = set(m.upper() for m in methods or [])
+
+        routes = []
+
+        for path in paths:
+            if self.trim_last_slash and isinstance(path, str):
+                path = path.rstrip('/')
+
+            path, pattern, params = parse_path(path)
+
+            if pattern:
+                route: Route = DynamicRoute(
+                    path, methods=methods, target=target, pattern=pattern, params=params)
+                self.dynamic.append(route)
+
+            else:
+                route = Route(path, methods, target)
+                self.plain[path].append(route)
+
+            routes.append(route)
+
+        return routes
+
+    def route(self, path: t.Union[CB, TYPE_PATH], *paths: TYPE_PATH,
+              methods: TYPE_METHODS = None, **opts) -> t.Callable:
+        """Register a route."""
+
+        def wrapper(target: CB) -> CB:
+            if hasattr(target, '__route__'):
+                target.__route__(self, *paths, methods=methods, **opts)
+                return target
+
+            if not self.validator(target):  # type: ignore
+                raise self.RouterError('Invalid target: %r' % target)
+
+            if not paths:
+                raise self.RouterError('Invalid route. A HTTP Path is required.')
+
+            self.bind(target, *paths, methods=methods, **opts)
+            return target
+
+        if isinstance(path, TYPE_PATH.__args__):  # type: ignore
+            paths = (path, *paths)
+
+        else:
+            return wrapper(path)  # type: ignore
+
+        return wrapper
+
+    def routes(self) -> t.List[BaseRoute]:
+        """Get a list of self routes."""
+        return sorted(self.dynamic + [r for routes in self.plain.values() for r in routes])
+
+    def __getattr__(self, method: str) -> t.Callable:
+        """Shortcut to the router methods."""
+        return partial(self.route, methods=method)
+
+
+from .routes import BaseRoute, RouteMatch, Route, DynamicRoute, Mount  # noqa
+
+# pylama: ignore=D
---
+++ http_router/routes.py
@@ -0,0 +1,110 @@
+import typing as t
+from urllib.parse import unquote
+
+from .router import Router
+from .utils import parse_path, INDENTITY
+
+
+class RouteMatch:
+    """Keeping route matching data."""
+
+    __slots__ = 'path', 'method', 'target', 'params'
+
+    def __init__(self, path: bool, method: bool,
+                 target: t.Any = None, params: t.Mapping[str, t.Any] = None):
+        self.path = path
+        self.method = method
+        self.target = target
+        self.params = params
+
+    def __bool__(self):
+        return self.path and self.method
+
+
+class BaseRoute:
+
+    __slots__ = 'path', 'methods'
+
+    def __init__(self, path: str, methods: t.Set = None):
+        self.path = path
+        self.methods = methods
+
+    def __lt__(self, route: 'BaseRoute'):
+        assert isinstance(route, BaseRoute), 'Only routes are supported'
+        return self.path < route.path
+
+    def match(self, path: str, method: str) -> RouteMatch:
+        raise NotImplementedError
+
+
+class Route(BaseRoute):
+    """Base plain route class."""
+
+    __slots__ = 'path', 'methods', 'target'
+
+    def __init__(self, path: str, methods: t.Set = None, target: t.Any = None):
+        super(Route, self).__init__(path, methods)
+        self.target = target
+
+    def match(self, path: str, method: str) -> RouteMatch:
+        """Is the route match the path."""
+        path_matched = path == self.path
+        method_matched = not self.methods or (method in self.methods)
+        if not path_matched and method_matched:
+            return RouteMatch(path_matched, method_matched)
+
+        return RouteMatch(path_matched, method_matched, self.target)
+
+
+class DynamicRoute(Route):
+    """Base dynamic route class."""
+
+    __slots__ = 'path', 'methods', 'target', 'pattern', 'params'
+
+    def __init__(self, path: str, methods: t.Set = None, target: t.Any = None,
+                 pattern: t.Pattern = None, params: t.Dict = None):
+        if pattern is None:
+            path, pattern, params = parse_path(path)
+            assert pattern, 'Invalid path'
+        self.pattern = pattern
+        self.params = params
+        super(DynamicRoute, self).__init__(path, methods, target)
+
+    def match(self, path: str, method: str) -> RouteMatch:
+        match = self.pattern.match(path)  # type: ignore  # checked in __post_init__
+        if not match:
+            return RouteMatch(False, False)
+
+        return RouteMatch(
+            True, not self.methods or method in self.methods, self.target, {
+                key: self.params.get(key, INDENTITY)(unquote(value))
+                for key, value in match.groupdict().items()
+            }
+        )
+
+
+class Mount(BaseRoute):
+    """Support for nested routers."""
+
+    __slots__ = 'path', 'methods', 'router', 'route'
+
+    def __init__(self, path: str, methods: t.Set = None, router: Router = None):
+        """Validate self prefix."""
+        self.router = router or Router()
+        self.route = self.router.route
+        path, pattern, _ = parse_path(path)
+        if pattern:
+            raise self.router.RouterError("Prefix doesn't support parameters")
+
+        if not path.startswith('/'):
+            raise self.router.RouterError("Prefix must start with /")
+
+        super(Mount, self).__init__(path.rstrip('/'), methods)
+
+    def match(self, path: str, method: str) -> RouteMatch:
+        """Is the route match the path."""
+        if not path.startswith(self.path):
+            return RouteMatch(False, False)
+
+        path = path[len(self.path):]
+        return self.router.match(path, method)
