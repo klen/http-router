4709d2657b5e5fb2a60e029ad45f186e29e0b71a
Kirill Klenov <horneds@gmail.com>
2021-04-20 15:42:36 +0300

    feat: wip

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 """Setup the library."""
 
+import os
+import sys
 from setuptools import setup, Extension
 
 
+NO_EXTENSIONS = (
+    sys.implementation.name != 'cpython' or
+    bool(os.environ.get("HTTP_ROUTER_NO_EXTENSIONS"))
+)
+EXT_MODULES = [] if NO_EXTENSIONS else [
+    Extension("http_router.router", ["http_router/router.c"], extra_compile_args=['-O2']),
+    Extension("http_router.routes", ["http_router/routes.c"], extra_compile_args=['-O2']),
+]
+print(NO_EXTENSIONS)
+print(EXT_MODULES)
+
 setup(
     setup_requires=["wheel"],
-    ext_modules=[
-        Extension("http_router.router", ["http_router/router.c"], extra_compile_args=['-O2']),
-        Extension("http_router.routes", ["http_router/routes.c"], extra_compile_args=['-O2']),
-    ]
+    ext_modules=EXT_MODULES,
 )
