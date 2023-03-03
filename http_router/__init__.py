5398dd69dc41eaf9bd81454794f182f37edeea9b
Kirill Klenov <horneds@gmail.com>
2023-03-04 00:08:51 +0700

    fix: wip

--- http_router/__init__.py
+++ http_router/__init__.py
@@ -1,7 +1,3 @@
-__version__ = "3.0.4"
-__license__ = "MIT"
-
-
 from .exceptions import MethodNotAllowed, NotFound, RouterError
 from .router import Router
 from .routes import DynamicRoute, Mount, PrefixedRoute, Route
