b9758ebd667385065ad0eb0070867332b0e73793
Kirill Klenov <horneds@gmail.com>
2023-03-03 23:53:35 +0700

    build: fix

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@
 import os
 import sys
 
-from Cython.Build import cythonize
 from setuptools import setup
 
-NO_EXTENSIONS = sys.implementation.name != "cpython" or bool(
-    os.environ.get("HTTP_ROUTER_NO_EXTENSIONS")
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+NO_EXTENSIONS = (
+    sys.implementation.name != "cpython"
+    or bool(os.environ.get("HTTP_ROUTER_NO_EXTENSIONS"))
+    or cythonize is None
 )
 
 print("*********************")
@@ -17,6 +24,6 @@ print("*********************")
 setup(
     setup_requires=["wheel"],
     ext_modules=[]
-    if NO_EXTENSIONS
+    if NO_EXTENSIONS or cythonize is None
     else cythonize("http_router/*.pyx", language_level=3),
 )
